8eef6d74c8cf4e8d1766709045f39d4f79508fed
vit-001 <vitaliy@inbox.ru>
2016-12-14 18:47:22 +0400

    14/12/2016

--- site_models/other/dude_model.py
+++ site_models/other/dude_model.py
@@ -11,13 +11,14 @@ class DudeSite(BaseSite):
         return "PornDude"
 
     def get_start_button_menu_text_url_dict(self):
-        return dict(Videos_Most_Recsent=URL('https://www.veronicca.com/videos?o=mr*'),
-                    Videos_Most_Viewed=URL('https://www.veronicca.com/videos?o=mv*'),
-                    Videos_Most_Commented=URL('https://www.veronicca.com/videos?o=md*'),
-                    Videos_Top_Rated=URL('https://www.veronicca.com/videos?o=tr*'),
-                    Videos_Top_Favorited=URL('https://www.veronicca.com/videos?o=tf*'),
-                    Videos_Longest=URL('https://www.veronicca.com/videos?o=lg*'),
-                    Channels=URL('https://www.veronicca.com/channels*')
+        return dict(tube_sites=URL('https://theporndude.com/top-porn-tube-sites*'),
+                    asian_tube_sites=URL('https://theporndude.com/top-asian-porn-tube-sites*'),
+                    arab_tube_sites=URL('https://theporndude.com/top-arab-porn-tube-sites*'),
+                    black_tube_sites=URL('https://theporndude.com/top-ebony-porn-tube-sites*'),
+                    indian_tube_sites=URL('https://theporndude.com/top-indian-porn-tube-sites*'),
+                    latin_tube_sites=URL('https://theporndude.com/top-latin-porn-tube-sites*'),
+                    fetish_tube_sites=URL('https://theporndude.com/top-fetish-porn-tube-sites*'),
+                    tgp_sites=URL('https://theporndude.com/best-porn-tgp-sites*')
                     )
 
     def startpage(self):
@@ -32,79 +33,16 @@ class DudeSite(BaseSite):
         startpage_rule = ParserRule()
         startpage_rule.add_activate_rule_level([('div', 'class', 'url_link_container')])
         startpage_rule.add_process_rule_level('a', {'href','title'})
-        startpage_rule.add_process_rule_level('img', {'src','alt'})
+        startpage_rule.add_process_rule_level('img', {'src'})
         # startpage_rule.set_attribute_modifier_function('href', lambda x: self.get_href(x,base_url))
-        startpage_rule.set_attribute_modifier_function('src', lambda x: self.get_href(x,base_url))
+        # startpage_rule.set_attribute_modifier_function('src', lambda x: self.get_href(x,base_url))
         parser.add_rule(startpage_rule)
-
-        startpage_pages_rule = ParserRule()
-        startpage_pages_rule.add_activate_rule_level([('ul', 'class', 'pagination')])
-        # startpage_pages_rule.add_activate_rule_level([('a', 'class', 'current')])
-        startpage_pages_rule.add_process_rule_level('a', {'href'})
-        startpage_pages_rule.set_attribute_modifier_function('href', lambda x: self.get_href(x,base_url))
-        parser.add_rule(startpage_pages_rule)
-
-        startpage_hrefs_rule = ParserRule()
-        startpage_hrefs_rule.add_activate_rule_level([('ul', 'class', 'drop2 hidden-xs')])
-        # startpage_hrefs_rule.add_activate_rule_level([('a', 'class', 'current')])
-        startpage_hrefs_rule.add_process_rule_level('a', {'href'})
-        # startpage_hrefs_rule.set_attribute_filter_function('href',lambda x: '/videos/' in x)
-        startpage_hrefs_rule.set_attribute_modifier_function('href', lambda x: self.get_href(x,base_url))
-        parser.add_rule(startpage_hrefs_rule)
-        #
-        video_rule = ParserRule()
-        video_rule.add_activate_rule_level([('div', 'class', 'video-container')])
-        video_rule.add_process_rule_level('source', {'src','label','res'})
-        # video_rule.set_attribute_filter_function('data', lambda text: 'video_url' in text)
-        video_rule.set_attribute_modifier_function('src',lambda x:self.get_href(x,base_url))
-        parser.add_rule(video_rule)
-        #
-        gallery_href_rule = ParserRule()
-        gallery_href_rule.add_activate_rule_level([('div', 'class', 'm-t-10 overflow-hidden')])
-        gallery_href_rule.add_process_rule_level('a', {'href'})
-        gallery_href_rule.set_attribute_modifier_function('href', lambda x: self.get_href(x,base_url))
-        parser.add_rule(gallery_href_rule)
-
-        gallery_user_rule = ParserRule(collect_data=True)
-        gallery_user_rule.add_activate_rule_level([('div', 'class', 'pull-left user-container')])
-        gallery_user_rule.add_process_rule_level('a', {'href'})
-        gallery_user_rule.set_attribute_modifier_function('href', lambda x: self.get_href(x,base_url))
-        gallery_user_rule.set_attribute_filter_function('href',lambda x:'#' not in x)
-        parser.add_rule(gallery_user_rule)
 
         self.proceed_parcing(parser, fname)
 
         result = ParseResult(self)
 
-        if video_rule.is_result(): #len(video_rule.get_result()) > 0:
-
-            urls=list()
-            for item in video_rule.get_result():
-                # print(item)
-                data = dict(text=item['res'], url=URL(item['src']))
-                urls.append(data)
-
-            if len(urls) == 1:
-                video = MediaData(urls[0]['url'])
-            elif len(urls) > 1:
-                video = MediaData(urls[-1]['url'])
-                for item in urls:
-                    video.add_alternate(item)
-            else:
-                return result
-
-            result.set_type('video')
-            result.set_video(video)
-
-            for f in gallery_user_rule.get_result(['data', 'href']):
-                result.add_control(ControlInfo('"'+f['data'].strip()+'"', URL(f['href'])))
-
-            for f in gallery_href_rule.get_result(['data', 'href']):
-                result.add_control(ControlInfo(f['data'], URL(f['href'])))
-
-            return result
-
-        if startpage_rule.is_result(): #len(startpage_rule.get_result()) > 0:
+        if startpage_rule.is_result():
             # result.set_type('hrefs')
             # hrefs=json.dumps(startpage_rule.get_result())
             # print(hrefs)
@@ -116,7 +54,10 @@ class DudeSite(BaseSite):
                     href=item['href']
 
                     if '/go/' in href:
-                        href='http://'+href.partition('/go/')[2]+'/'
+                        href='http://'+href.partition('/go/')[2]
+
+                    if not href.endswith('/'):
+                        href+='/'
 
                     model=self.model
 
@@ -131,14 +72,6 @@ class DudeSite(BaseSite):
                     # result.add_thumb(ThumbInfo(thumb_url=URL(item['src']), href=URL(item['href']),description=item.get('alt',item.get('title',''))))
 
             print('Accepted=',acc)
-
-            for item in startpage_pages_rule.get_result(['href', 'data']):
-                result.add_page(ControlInfo(item['data'], URL(item['href'])))
-
-            for item in startpage_hrefs_rule.get_result(['href', 'data']):
-                # print(item)
-                label=item['href'].strip('*').rpartition('/')[2]
-                result.add_control(ControlInfo(label, URL(item['href'])))
 
         return result
 
